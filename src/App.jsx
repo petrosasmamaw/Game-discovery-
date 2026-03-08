c73a3db3da5e427d86c28413972633162b2cfd7f
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-03-08 23:06:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavBar from './features/components/navBar.jsx'
 import GameList from './features/games/gameList.jsx';
 import GameDetail from './features/games/gameDetail.jsx';
 import FavoritesList from './features/favorites/favoritesList.jsx';
-1234567
+
 function App() {
   return (
     <BrowserRouter>
